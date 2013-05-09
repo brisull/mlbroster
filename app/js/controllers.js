049528f12c824340b4cd415bc892e215be42f192
Brian Sullivan <bri_sull@yahoo.com>
2013-05-08 17:56:06 -0700

    controllers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,64 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+function ListCtrl($scope, Players) {
+    // Start with a list of all players
+    var allPlayers = Players.query(getTeams);
 
-  }])
-  .controller('MyCtrl2', [function() {
+    // Get just the one we want
+	function getTeams() {
+		var teams = $scope.teams = [];
 
-  }]);
+		// filter
+		angular.forEach(allPlayers, function(item, key) {
+		  if ( teams.indexOf(item.team_abbrev) > -1) {
+			return;
+			}
+			teams.push(item.team_abbrev);
+		});
+	}
+    $scope.orderProp = 'team_abbrev';
+    $scope.foo = 'bar';
+}
+
+function PlayerCtrl($scope, $routeParams, Players) {
+
+	// Start with a list of all players
+    var allPlayers = Players.query(getPlayer);
+
+    // Get just the one we want
+	function getPlayer() {
+		$scope.player = {};
+
+		// filter
+		angular.forEach(allPlayers, function(item, key) {
+			console.log(item.player_id);
+		  if ($routeParams.player_id.length && $routeParams.player_id == item.player_id ) { 
+		  	$scope.player = item;
+		  	return; 
+		  }
+		});
+	}
+}
+
+function TeamCtrl($scope, $routeParams, Players) {
+	// Start with a list of all players
+    var allPlayers = Players.query(getTeamPlayers);
+
+    $scope.params = $routeParams;
+    $scope.team = $routeParams.team_abbrev;
+    // Get just the one we want
+	function getTeamPlayers() {
+		$scope.players = [];
+
+		// filter
+		angular.forEach(allPlayers, function(item, key) {
+			// console.log(item.player_id);
+			if ($routeParams.team_abbrev.length && $routeParams.team_abbrev !== item.team_abbrev ) { 
+				return; 
+			}
+			$scope.players.push(item);
+		});
+	}
+
+}
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,14 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
+angular.module('mlbRoster.services', []).
   value('version', '0.1');
+
+angular.module('mlbRoster.services', ['ngResource']).
+    factory('Players', function($resource){
+        return $resource('data/mlb_rosters_3.json', {}, {
+            query: {method:'GET', params:{}, isArray:true}
+        });
+//
+//        return $resource('/api/restaurant/:id', {id: '@id'});
+    });
